56719b7d2de6dd4441c1c7eb7aa3cc0c982c23b9
Ellen Steiner <ellen@steiner-mercker.de>
2026-03-06 12:07:33 +0100

    started new sorting algorythm

--- operations/rotating.c
+++ operations/rotating.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   rotating.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: esteiner <esteiner@student.42.fr>          +#+  +:+       +#+        */
+/*   By: esteiner <esteiner@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 22:03:40 by esteiner          #+#    #+#             */
-/*   Updated: 2023/06/05 20:00:15 by esteiner         ###   ########.fr       */
+/*   Updated: 2026/03/05 17:26:53 by esteiner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,10 @@ void	rotate_b(t_swag **stack_b)
 	write(1, "rb\n", 3);
 }
 
-/* rotates both stacks so that the first node is now the last one */
+/// @brief rotates both stacks so that the first node is now the last one 
+/// @param stack_a pointer to the first node of stack a
+/// @param stack_b pointer to the first node of stack b
+/// @return nothing, but writes "rr\n" to the standard output
 void	rotate_both(t_swag **stack_a, t_swag **stack_b)
 {
 	t_swag	*temp_node;
